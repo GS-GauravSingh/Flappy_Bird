a5bc315487e13aa20437a0797c6c0ae70f9c9fe1
Gaurav <gs062254@gmail.com>
2021-10-09 14:53:41 +0530

    added moving pipes and bird animation

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+from os import pipe
 import pygame, pygame.display, pygame.image, pygame.transform, pygame.event, pygame.font, pygame.time, pygame.mixer
+from pygame.constants import K_SPACE
 import sys, random
 pygame.init()
 pygame.mixer.init()
@@ -16,20 +18,33 @@ Game_Sounds = {} # Dictionary to store all sounds as key value pairs
 clock = pygame.time.Clock()
 font = pygame.font.SysFont("footlight", 40)
 base_x = 0
+bird_index = 0
+gravity = 1
+bird_movement = 0
+pipe_list = []
+pipe_height = [400, 320, 500, 250]
 
 """ Loading and Adding game images to dictionary """
 Game_Imges["background"] = pygame.image.load("images/background.png") # Background
 Game_Imges["base"] = pygame.image.load("images/base1.png") # Base
 
 # Small size bird images for main game animation
-Game_Imges["upflap1"] = pygame.image.load("images/upflap1.png") 
-Game_Imges["midflap1"] = pygame.image.load("images/midflap1.png")
-Game_Imges["downflap1"] = pygame.image.load("images/downflap1.png")
+Game_Imges["main_game_bird"] = [
+
+    pygame.image.load("images/upflap1.png"),
+    pygame.image.load("images/midflap1.png"),
+    pygame.image.load("images/downflap1.png")
+
+]
 
 # Large bird images for welcome screen animation
-Game_Imges["upflap"] = pygame.image.load("images/upflap.png") 
-Game_Imges["midflap"] = pygame.image.load("images/midflap.png")
-Game_Imges["downflap"] = pygame.image.load("images/downflap.png")
+Game_Imges["welcome_screen_bird"] = [
+
+    pygame.image.load("images/upflap.png"),
+    pygame.image.load("images/midflap.png"),
+    pygame.image.load("images/downflap.png")
+    
+]
 
 # Pipes
 Game_Imges["pipe"] = pygame.image.load("images/pipe.png")
@@ -47,10 +62,51 @@ def moving_base():
     GameWindow.blit(Game_Imges["base"], (base_x, 500))
     GameWindow.blit(Game_Imges["base"], (base_x + 333, 500))
     if base_x > -333:
-        base_x -= 2
+        base_x -= 3
     else:
         base_x = 0
+    
 
+def bird_animation():
+    global Bird_rect
+    new_bird = Game_Imges["main_game_bird"][bird_index]
+    new_bird_rect = new_bird.get_rect()
+    new_bird_rect.center = (80, Bird_rect.centery)
+    return new_bird, new_bird_rect
+
+
+def creating_pipes():
+    random_pipe = random.choice(pipe_height)
+    lower_pipe = Game_Imges["pipe"].get_rect(midtop = (350, random_pipe ))
+    upper_pipe = Game_Imges["pipe"].get_rect(midbottom = (350, random_pipe - 200 ))
+    return upper_pipe, lower_pipe
+
+
+def blitting_pipes(p_list):
+    """ Function for blitting pipes on screen """
+    for pipes in p_list:    
+        if pipes.bottom >= 540:
+            GameWindow.blit(Game_Imges["pipe"], pipes)
+        else:
+            flip_pipe = pygame.transform.flip(Game_Imges["pipe"], False, True)
+            GameWindow.blit(flip_pipe, pipes)
+
+
+def pipes_velocity(p_list):
+    """ This function returns a new list in which pipes are moving in -x directiuon """
+    for pipes in p_list:
+        pipes.centerx -= 4
+    return p_list
+
+""" Rects """
+bird_select = Game_Imges["main_game_bird"][bird_index]
+Bird_rect = bird_select.get_rect(center = (80, 200))
+Bird_flap = pygame.USEREVENT
+pygame.time.set_timer(Bird_flap, 200)
+
+# Pipes
+Spawn_Pipe = pygame.USEREVENT + 1
+pygame.time.set_timer(Spawn_Pipe, 1200)
 
 
 while True:
@@ -58,11 +114,44 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        
+        if event.type == pygame.KEYDOWN:
+            if event.key == K_SPACE:
+                bird_movement = 0
+                bird_movement -= 15
+                Game_Sounds["wing"].play()
+        
+        if event.type == Bird_flap:
+            if bird_index <= 1:
+                bird_index += 1
+            else:
+                bird_index = 0
+
+        if event.type == Spawn_Pipe:
+            pipe_list.extend(creating_pipes())
+            
+            
 
 
     GameWindow.blit(Game_Imges["background"], (0, 0))
+
+    # Bird
+    bird_movement += gravity
+    Bird_rect.centery += bird_movement
+    bird_select, Bird_rect = bird_animation()
+    GameWindow.blit(bird_select, Bird_rect)
+
+    #Pipes
+    new_pipe_list = pipes_velocity(pipe_list)
+    blitting_pipes(new_pipe_list)
+
+    # Base
     moving_base()
 
+     
+    
+
+    
 
     pygame.display.update()
     clock.tick(FPS)
